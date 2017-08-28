54563575df68d5615d90d0b704902b3b7b71d9d2
MAlvinR <MAlvinR@users.noreply.github.com>
2017-08-28 14:27:45 +0700

    Handle network request using Volley

--- app/src/main/java/id/co/horveno/discovermovies/fragment/NowPlayingFragment.kt
+++ app/src/main/java/id/co/horveno/discovermovies/fragment/NowPlayingFragment.kt
@@ -3,6 +3,7 @@ package id.co.horveno.discovermovies.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +13,17 @@ import com.android.volley.Response
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import id.co.horveno.discovermovies.EndPoint
 
 import id.co.horveno.discovermovies.R
+import id.co.horveno.discovermovies.gson.NowPlaying
 import kotlinx.android.synthetic.main.now_playing_fragment.*
 
 class NowPlayingFragment : Fragment() {
 
+    var nowPlayingGson:NowPlaying? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view =  inflater!!.inflate(R.layout.now_playing_fragment, container, false)
@@ -32,9 +37,22 @@ class NowPlayingFragment : Fragment() {
 
     private fun getNowPlayingData() {
         val requestQueue: RequestQueue = Volley.newRequestQueue(activity)
-        val stringRequest: StringRequest = StringRequest(Request.Method.GET, EndPoint.URL_UPCOMING, Response.Listener { s ->
+        val stringRequest: StringRequest = StringRequest(Request.Method.GET, EndPoint.URL_UPCOMING, object:Response.Listener<String> {
 
-        }, Response.ErrorListener { error: VolleyError? ->
+            override fun onResponse(response: String?) {
+
+                var gsonBuilder:GsonBuilder = GsonBuilder()
+                var gson:Gson = gsonBuilder.create()
+
+                nowPlayingGson = gson.fromJson(response, NowPlaying::class.java)
+
+
+            }
+
+        }, object:Response.ErrorListener {
+            override fun onErrorResponse(error: VolleyError?) {
+                Log.e("NowPlayingFragment", error.toString())
+            }
 
         })
         requestQueue.add<String>(stringRequest)
--- app/src/main/java/id/co/horveno/discovermovies/fragment/UpcomingFragment.kt
+++ app/src/main/java/id/co/horveno/discovermovies/fragment/UpcomingFragment.kt
@@ -3,6 +3,7 @@ package id.co.horveno.discovermovies.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,17 +13,22 @@ import com.android.volley.Response
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 
 import id.co.horveno.discovermovies.R
 import id.co.horveno.discovermovies.EndPoint
+import id.co.horveno.discovermovies.gson.Upcoming
 import kotlinx.android.synthetic.main.upcoming_fragment.*
 
 class UpcomingFragment : Fragment() {
 
+    var upcomingGson:Upcoming? = null
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val upcomingView = inflater!!.inflate(R.layout.upcoming_fragment, container, false)
 
-        var linearLayoutManager = LinearLayoutManager(activity)
+        val linearLayoutManager = LinearLayoutManager(activity)
 
         recyclerUpcoming.layoutManager = linearLayoutManager
         getUpcomingData()
@@ -31,11 +37,19 @@ class UpcomingFragment : Fragment() {
 
     private fun getUpcomingData() {
         val requestQueue: RequestQueue = Volley.newRequestQueue(activity)
-        val stringRequest: StringRequest = StringRequest(Request.Method.GET,EndPoint.URL_NOWPLAYING, Response.Listener { s ->
+        val stringRequest: StringRequest = StringRequest(Request.Method.GET,EndPoint.URL_NOWPLAYING, object:Response.Listener<String> {
+            override fun onResponse(response: String?) {
+                val gsonBuilder:GsonBuilder = GsonBuilder()
+                val gson:Gson = gsonBuilder.create()
+
+                upcomingGson = gson.fromJson(response, Upcoming::class.java)
+            }
 
 
-
-        }, Response.ErrorListener { error: VolleyError? ->
+        }, object:Response.ErrorListener {
+            override fun onErrorResponse(error: VolleyError?) {
+                Log.e("UpcomingFragment", error.toString())
+            }
 
         })
                 requestQueue.add<String>(stringRequest)
